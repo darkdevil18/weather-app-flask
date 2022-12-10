0bced1425402f29732f462d1b6f8cbe1e0dbc90a
darkdevil18 <sampaddibya@gmail.com>
2022-12-10 10:53:45 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,43 +4,23 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+api_url = os.environ['api_url']
 
-def get_current_weather_report(city: str):
-    # url = "https://weatherapi-com.p.rapidapi.com/current.json"
-    url = os.environ['api_url']
+api_key = os.environ['api_key']
+api_host = os.environ['api_host']
 
-    api_key = os.environ['api_key']
-    api_host = os.environ['api_host']
 
-    querystring = {"q": city}
+def two_day_forecast(city: str):
+
+    querystring = {"q": city, "days": "2"}
+
     headers = {
         "X-RapidAPI-Key": api_key,
         "X-RapidAPI-Host": api_host
     }
 
     response = requests.request(
-        "GET", url, headers=headers, params=querystring)
-
-    return response.json()
-
-
-def two_day_forecast(city: str):
-    url = "https://weatherapi-com.p.rapidapi.com/forecast.json"
-
-    url = os.environ['api_url']
-
-    api_key = os.environ['api_key']
-    api_host = os.environ['api_host']
-
-    querystring = {"q": city, "days": "2"}
-
-    headers = {
-        "X-RapidAPI-Key": "5aebfe91bamsh1532d00e7634804p168ad6jsn9d73ee4e86f6",
-        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
-    }
-
-    response = requests.request(
-        "GET", url, headers=headers, params=querystring)
+        "GET", api_url, headers=headers, params=querystring)
 
     return response.json()
 
